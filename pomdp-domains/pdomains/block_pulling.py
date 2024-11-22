5ca14dc694b22a535eef1599a84842db631ee9c5
Ben <522181941@qq.com>
2024-11-22 12:42:07 +0800

    update block pull

--- pomdp-domains/pdomains/block_pulling.py
+++ pomdp-domains/pdomains/block_pulling.py
@@ -12,8 +12,8 @@ import math
 import torch
 
 class BlockEnv(gym.Env):
-    def __init__(self, seed=0, img_size=84, rendering=False, robot='kuka', action_sequence='pxyzr', noise=False):
-
+    def __init__(self, seed=0, img_size=84, rendering=False, robot='kuka', action_sequence='pxyzr', noise=False, obs_dict=False):
+        self._obs_dict = obs_dict
         workspace = np.asarray([[0.3, 0.7],
                                 [-0.2, 0.2],
                                 [0.01, 0.25]])
@@ -164,9 +164,10 @@ class BlockEnv(gym.Env):
             obs[0] += 0.007*self.rand_perlin_2d((self.image_size, self.image_size), (
                 (np.random.choice([1, 2, 4, 6], 1)[0]),
                 int(np.random.choice([1, 2, 4, 6], 1)[0]))).numpy()
-
-        state_tile = state*np.ones((1, obs.shape[1], obs.shape[2]))
-        stacked = np.concatenate([obs, state_tile], axis=0)
+        depths = [v for k,v in obs.items()]
+        depth = np.min(np.stack(depths, axis=0), axis=0)
+        state_tile = state*np.ones(depth.shape)
+        stacked = np.stack([depth, state_tile], axis=0)
         return stacked
 
     def step(self, action):
@@ -179,7 +180,8 @@ class BlockEnv(gym.Env):
             action[0] = 0.5 * (action[0] + 1)  # [-1, 1] to [0, 1] for p
         (state, _, obs), reward, done = self.core_env.step(action)
 
-        self.obs = self._process_obs(state, obs)
+        self.obs = obs.copy()
+        self.obs['image'] = self._process_obs(state, obs['depth'])
 
         info = {}
 
@@ -190,8 +192,8 @@ class BlockEnv(gym.Env):
 
         if self.show:
             self.render()
-
-        return self.obs, reward, done, info
+        
+        return self.obs if self._obs_dict else self.obs['image'], reward, done, info
 
     def render(self, mode='human'):
         pass
@@ -200,15 +202,15 @@ class BlockEnv(gym.Env):
         self.target_obj_idx = 1 - self.target_obj_idx
         self.step_cnt = 0
         (state, _, obs) = self.core_env.reset(self.target_obj_idx, noise=self.include_noise)
-        self.obs = self._process_obs(state, obs)
+        self.obs = obs.copy()
+        self.obs['image'] = self._process_obs(state, obs['depth'])
 
         # if self.old_obs is not None:
         #     diff = obs[0] - self.old_obs
         #     print(np.min(diff), np.max(diff), np.mean(diff))
 
         # self.old_obs = obs
-
-        return self.obs
+        return self.obs if self._obs_dict else self.obs['image']
 
     def close(self):
         self.core_env.close()
